6c68d138af660eafc846a36093134c1cbbfc5156
frantecbh <franciscomenezesbh@gmail.com>
2022-10-13 17:26:22 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ export function App() {
         <h1 className='font-bold text-2xl text-gray-900'>Ola Frantec</h1>
 
 <button className='bg-cyan-500 font-bold px-4 py-2 text-gray-800 rounded-sm'>Enviar</button>
+
+
     </>
 
   )
